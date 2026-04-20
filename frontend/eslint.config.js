9037371da5835ea5438bcda1103d22d8f157426c
tvquan2004-svg <tvquan2004@gmail.com>
2026-04-21 02:07:57 +0700

    fix bug lint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,7 +23,11 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }],
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
